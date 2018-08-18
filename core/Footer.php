2c25d66690e96af12af2b6b93c487d22d9c8bb83
John Wu <524306184@qq.com>
2018-08-18 23:56:30 +0800

    :sparkles: Footer 加入小工具信息
    :sparkles: Footer 加入小工具信息

--- core/Footer.php
+++ core/Footer.php
@@ -1,14 +1,23 @@
+        <!-- FOOTER -->
+        <hr>
+<footer class="footer text-center text-muted">
+<?php
+    echo($mLoader->module['Name'] . ' / Developed by <a href="' . $mLoader->module['AuthorURI'] . '"><b>' . $mLoader->module['Author'] . '</b></a>');?>
+<br>
+    Cube · Create everything you want!
+</footer>
         <!-- MODULE END HERE -->
     </div>
 </div>
-    </body>
-    <script src="/static/lib/jquery.niftymodals/js/jquery.niftymodals.js" type="text/javascript"></script>
-    <script>
-    $.fn.niftyModal('setDefaults',{
-        overlaySelector: '.modal-overlay',
-        contentSelector: '.modal-content',
-        closeSelector: '.modal-close',
-        classAddAfterOpen: 'modal-show'
-    });
-    </script>
+
+</body>
+<script src="/static/lib/jquery.niftymodals/js/jquery.niftymodals.js" type="text/javascript"></script>
+<script>
+$.fn.niftyModal('setDefaults',{
+    overlaySelector: '.modal-overlay',
+    contentSelector: '.modal-content',
+    closeSelector: '.modal-close',
+    classAddAfterOpen: 'modal-show'
+});
+</script>
 </html>
