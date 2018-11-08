c236ed4d1a3cb8616e13c2ca698fcca677c07fa0
John Wu <524306184@qq.com>
2018-11-08 17:33:58 +0800

    Account UI 更新

--- core/module/Account/Account_Login.php
+++ core/module/Account/Account_Login.php
@@ -1,23 +1,13 @@
-<div class="main-content container-fluid">
-    <div class="splash-container">
-    <div class="card card-border-color card-border-color-primary">
-        <div class="card-header"><span class="splash-description">请先登录 Cube</span></div>
-        <div class="card-body">
-        <form action="/Account/LoginAction" method="post" accept-charset="utf-8">
-            <div class="form-group">
-            <input class="form-control" name="Name" type="text" placeholder="账号" autocomplete="off">
-            </div>
-            <div class="form-group">
-            <input class="form-control" name="Passwd" type="password" placeholder="密码">
-            </div>
-            <div class="form-group row login-tools">
-            <div class="col-6 login-remember">
-            </div>
-            <div class="col-6 login-forgot-password"><a href="pages-forgot-password.html">忘记密码?</a></div>
-            </div>
-            <div class="form-group login-submit"><button type="submit" class="btn btn-primary btn-xl" data-dismiss="modal">登录 Cube</button></div>
-        </form>
+<div class="uk-align-center uk-card uk-card-default uk-card-body uk-width-1-3@m">
+    <h3 class="uk-card-title">请先登录 Cube</h3>
+    <form action="/Account/LoginAction" method="post" accept-charset="utf-8">
+        <div class="uk-margin">
+            <input class="uk-input" name="Name" type="text" placeholder="账号" autocomplete="off">
         </div>
-    </div>
-    </div>
+        <div class="uk-margin">
+            <input class="uk-input" name="Passwd" type="password" placeholder="密码">
+        </div>
+
+        <div class="uk-margin"><button type="submit" class="uk-button uk-button-primary">登录 Cube</button></div>
+    </form>
 </div>
