2b2a4ee6713161a503765064be21be03d807bb19
John Wu <524306184@qq.com>
2018-11-08 18:07:40 +0800

    Data UI 更新

--- core/module/Data/Data_mainpage.php
+++ core/module/Data/Data_mainpage.php
@@ -1,48 +1,28 @@
-<div class="col-sm-12">
-    <div class="card card-table">
-    <div class="card-header">数据</div>
-    <div class="card-body">
-        <div class="table-responsive noSwipe">
-        <table class="table table-striped table-hover">
-            <thead>
-            <tr>
-                <th style="width:4%;">
-                <label class="custom-control custom-control-sm custom-checkbox">
-                    <input class="custom-control-input" type="checkbox"><span class="custom-control-label"></span>
-                </label>
-                </th>
-                <th style="width:15%;">名称</th>
-                <th>数据</th>
-                <th style="width:10%;"></th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach($storageDataList as $Item){?>
-                <tr>
-                    <td>
-                    <label class="custom-control custom-control-sm custom-checkbox">
-                        <input class="custom-control-input" type="checkbox"><span class="custom-control-label"></span>
-                    </label>
-                    </td>
+<table class="uk-table uk-table-striped">
+    <thead>
+        <tr>
+            <th style="width:15%;">名称</th>
+            <th>数据</th>
+            <th style="width:15%;"></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($storageDataList as $Item){?>
+        <tr>
+            <td class="cell-detail">
+                <span>
+                <?php echo($Item['Storage_ModuleName']);?>
+                </span>
+            </td>
 
-                    <td class="cell-detail">
-                        <span>
-                        <?php echo($Item['Storage_ModuleName']);?>
-                        </span>
-                    </td>
+            <td class="cell-detail">
+            <?php echo($Item['Storage_Data']);?>
+            </td>
 
-                    <td class="cell-detail">
-                    <?php echo($Item['Storage_Data']);?>
-                    </td>
-
-                    <td class="cell-detail">
-                        <a href="/Data/CleanData?ModuleName=<?php echo($Item['Storage_ModuleName']);?>">删除所有</a>
-                    </td>
-                <?php } ?>
-                </tr>
-            </tbody>
-        </table>
-        </div>
-    </div>
-    </div>
-</div>
+            <td class="cell-detail">
+                <a class="uk-button uk-button-danger" href="/Data/CleanData?ModuleName=<?php echo($Item['Storage_ModuleName']);?>">删除所有</a>
+            </td>
+        <?php } ?>
+        </tr>
+    </tbody>
+</table>
